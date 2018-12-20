23f635554b8cbea47c6edf21b5574da3d4034e8d
weisheng_cheng <maxmaman@gmail.com>
2018-12-20 16:52:50 +0800

    Add solution of 700. Search in a Binary Search Tree
    There are 2 solution I wrote.
    1 used iteration.
    1 used recursion.
    
    TODO:
    1.Write parsing string to tree object method
    2.Write print tree method
    3.Write python unit test

---
+++ binary_tree/700_searchBST.py
@@ -0,0 +1,53 @@
+from binary_tree import TreeNode
+
+
+class Solution(object):
+
+    def searchBST_by_recursion(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        while True:
+            if root:
+                if root.val == val:
+                    return root
+                if root.val > val:
+                    root = root.left
+                else:
+                    root = root.right
+            else:
+                return None
+
+    def searchBST_by_iteration(self, root, val):
+        """
+        :type root: TreeNode
+        :type val: int
+        :rtype: TreeNode
+        """
+        if not root:
+            return None
+        if root.val == val:
+            return root
+        elif root.val > val:
+            return self.searchBST_by_iteration(root.left, val)
+        else:
+            return self.searchBST_by_iteration(root.right, val)
+
+# TODO build tree from string method
+# testCase
+#    20
+#   /  \
+#  12  25
+a = TreeNode(20)
+a.left = TreeNode(12)
+a.right = TreeNode(25)
+
+public_method_names = [method for method in dir(Solution) if callable(getattr(Solution, method)) if not method.startswith('_')]  # 'private' methods start from _
+x = Solution()
+for method in public_method_names:
+    # TODO print tree method
+    getattr(x, method)(a, 12)  # call
+
+# TODO Unit Test
